52f39b54ba13ec447522c81e0b8b9cf93a7334d2
afnanjasim <ryzenfluke@gmail.com>
2024-01-21 23:28:18 +0530

    more changes

--- variables.tf
+++ variables.tf
@@ -3,17 +3,20 @@ variable "users" {
   type = map(object({
     name = string
     devuser = bool
+    qauser = bool
     
   }))
   default = {
     devuser = {
       name = "devuser"
       devuser="1"
+      qauser="0"
       
     }
     qauser =  {
       name = "qauser" 
       devuser = "0"
+      qauser="1"
       
     }
   }
